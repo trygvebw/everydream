88e804b6a7120e021bb887856b9abc1965fd5ba6
Victor Hall <victor.charles.hall@gmail.com>
2022-11-03 20:30:46 -0400

    fix test_dl.py script

--- ldm/data/test_dl.py
+++ ldm/data/test_dl.py
@@ -1,8 +1,10 @@
+# script to test data loader
+# python ldm/data/test_dl.py
 import data_loader
 
 data_root = "r:/everydream-trainer/training_samples/multiaspect"
 
-data_loader = data_loader.DataLoaderMultiAspect(data_root=data_root, seed=555, debug=1)
+data_loader = data_loader.DataLoaderMultiAspect(data_root=data_root, seed=555, debug_level=2)
 
 image_caption_pairs = data_loader.get_all_images()
 
